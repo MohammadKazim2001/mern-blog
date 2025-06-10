4f132921f35cea8bf0931ab0a13336169cc65ef3
Kazim Amiri <mohammad.kazimamiri2001@gmail.com>
2025-06-10 22:14:25 +0430

    product model and controller createed

---
+++ api/controllers/product.controller.js
@@ -0,0 +1,8 @@
+export const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch products" });
+  }
+};
